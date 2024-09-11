c91e051a63391606d6bb2cda09fe1c656fea8500
Jeet <jetdeula@gmail.com>
2024-09-11 16:40:16 +0545

    updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,8 @@ import { useAnimation, motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
-type Props = {};
 
-export default function Navbar({}: Props) {
+export default function Navbar() {
   const location = useLocation();
   const [scrollingUp, setScrollingUp] = useState(true);
   const controls = useAnimation();
@@ -47,12 +46,12 @@ export default function Navbar({}: Props) {
       className="w-full flex  justify-center items-center fixed top-0 left-0 py-[0.5vw]   z-[50] "
     >
       <div className="mx-auto flex justify-center rounded-full px-[2vw] py-[0.5vw] bg-zinc-100 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] items-center">
-        <div className="w-full gap-[10vw] flex justify-between items-center">
+        <div className="w-full flex justify-center gap-28 items-center">
           <Link to="">
-            <img className="w-[5vw]" src="/public/logo/nice-logo.png" alt="" />
+            <img className="w-[6vw]" src="/public/logo/nice-logo.png" alt="" />
           </Link>
 
-          <div className="flex gap-5 text-[0.9vw] font-semibold justify-center items-center text-zinc-700">
+          <div className="flex gap-[2vw] text-[0.9vw] font-semibold justify-center items-center text-zinc-700">
             {navItems.map(({ href, label }) => (
               <Link key={href} to={href}>
                 <span
@@ -64,6 +63,7 @@ export default function Navbar({}: Props) {
                 </span>
               </Link>
             ))}
+              </div>
             {/* <Link to="" className="hover:text-secondary-500">
               Home
             </Link>
@@ -85,7 +85,8 @@ export default function Navbar({}: Props) {
                 Contact
               </button>
             </Link>
-          </div>
+
+        
         </div>
       </div>
     </motion.div>
@@ -97,5 +98,5 @@ const navItems = [
   { href: "/about", label: "About" },
   { href: "/services", label: "Services" },
   { href: "/blogs", label: "Blogs" },
-  { href: "/jobs", label: "Find Jobs" },
+  { href: "/jobs", label: "Jobs" },
 ];
---
+++ src/components/website/homepage/Ctaform.tsx
@@ -0,0 +1,114 @@
+import React from "react";
+
+const Ctaform = ({closeDropdown}) => {
+  return (
+    <main className="bg-white rounded-2xl  p-10">
+        <img src="./public/cancelbtn.svg" alt="cancel-btn" className="absolute right-6 top-6 cursor-pointer" onClick={closeDropdown} />
+      <div>
+        <p className="italic text-[14px] text-center w-[60%] mx-auto">
+          We specialize in providing skilled professionals across industries
+          like construction, oil and gas, hospitality, and more, we’re here to
+          help you find the right talent.ँ
+        </p>
+        <h3 className="font-medium text-[20px] text-center py-4 ">
+          Contact us today to access top talent for your business needs.
+        </h3>
+        <div className="w-[50em] bg-[#2088CA] mx-auto h-[1.2px]"> </div>
+      </div>
+      <form action="" className="col-span-3  py-5 mt-5 grid grid-cols-2 gap-5 gap-x-24">
+        <div className="flex flex-col gap-2">
+          <label
+            htmlFor="firstName"
+            className="font-medium text-[3.5vw] md:text-[2.5vw] text-tertiary-600 lg:text-[0.9vw]"
+          >
+            First Name <span className="text-blue-500">*</span>
+          </label>
+          <input
+            type="text"
+            id="firstName"
+            placeholder="Your First Name"
+            name="firstName"
+            className="p-2 py-2 lg:py-4 border-2 rounded-xl border-zinc-200 outline-none bg-transparent"
+            required
+          />
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <label
+            htmlFor="lastName"
+            className="font-medium text-[3.5vw] md:text-[2.5vw] text-tertiary-600 lg:text-[0.9vw]"
+          >
+            Last Name <span className="text-blue-500">*</span>
+          </label>
+          <input
+            type="text"
+            id="lastName"
+            placeholder="Your Last Name"
+            name="lastName"
+            className="p-2 py-2 lg:py-4 border-2 rounded-xl border-zinc-200 outline-none bg-transparent"
+            required
+          />
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <label
+            htmlFor="email"
+            className="font-medium text-[3.5vw] md:text-[2.5vw] text-tertiary-600 lg:text-[0.9vw]"
+          >
+            Email <span className="text-blue-500">*</span>
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Your Email"
+            className="p-2 py-2 lg:py-4 border-2 rounded-xl border-zinc-200 outline-none bg-transparent"
+            required
+          />
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <label
+            htmlFor="phone"
+            className="font-medium text-[3.5vw] md:text-[2.5vw] text-tertiary-600 lg:text-[0.9vw]"
+          >
+            Phone <span className="text-blue-500">*</span>
+          </label>
+          <input
+            type="tel"
+            id="phone"
+            name="phone"
+            placeholder="Your Phone Number"
+            className="p-2 py-2 lg:py-4 border-2 rounded-xl border-zinc-200 outline-none bg-transparent"
+            required
+          />
+        </div>
+
+        <div className="col-span-2 flex flex-col gap-2">
+          <label
+            htmlFor="message"
+            className="font-medium text-[3.5vw] md:text-[2.5vw] text-tertiary-600 lg:text-[0.9vw]"
+          >
+            Message <span className="text-blue-500">*</span>
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Your Message"
+            rows="4"
+            className="p-2 py-2 lg:py-4 border-2 rounded-xl border-zinc-200 outline-none bg-transparent"
+            required
+          />
+        </div>
+
+        <div className="flex justify-start">
+          <button className="px-[5vw] lg:px-[2vw] py-[2vw] lg:py-[0.8vw] text-[3.5vw] lg:text-[1vw] font-semibold bg-orange-500 hover:bg-orange-600 duration-300 rounded-full text-zinc-50  border-zinc-600 ">
+            Submit
+          </button>
+        </div>
+      </form>
+    </main>
+  );
+};
+
+export default Ctaform;
