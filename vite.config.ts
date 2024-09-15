d8eb7fd36d0d4ee780ca1b009ba268a8118fb193
Jeet <jetdeula@gmail.com>
2024-09-15 15:28:05 +0545

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import imagemin from 'vite-plugin-imagemin';
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    imagemin({
-      gifsicle: {
-        optimizationLevel: 7,
-        interlaced: false,
-      },
-      optipng: {
-        optimizationLevel: 7,
-      },
-      mozjpeg: {
-        quality: 80,
-      },
-      pngquant: {
-        quality: [0.65, 0.8],
-        speed: 4,
-      },
-      svgo: {
-        plugins: [
-          { name: 'removeViewBox', active: false },
-          { name: 'removeEmptyAttrs', active: false },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
 })
