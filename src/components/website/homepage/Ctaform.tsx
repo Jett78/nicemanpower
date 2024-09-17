7d26731dff74dd4b3ca1adddce02e3830a31c155
Jeet <jetdeula@gmail.com>
2024-09-17 11:17:44 +0545

    changes

--- src/components/website/homepage/Ctaform.tsx
+++ src/components/website/homepage/Ctaform.tsx
@@ -2,14 +2,14 @@
 const Ctaform = ({ closeDropdown }: { closeDropdown: () => void }) => {
   return (
     <main className="bg-white sm:rounded-2xl  md:p-10 p-6 absolute left-1/2 -translate-x-1/2 top-1/2  -translate-y-1/2  lg:w-[50em]  mx-auto">
-        <img src="./public/cancelbtn.svg" alt="cancel-btn" className="absolute right-6 md:top-6 top-2 cursor-pointer md:w-8 w-6" onClick={closeDropdown} />
+        <img src="./public/cancelbtn.svg" alt="cancel-btn" className="absolute right-6 md:top-6 top-2 cursor-pointer md:w-8 w-4" onClick={closeDropdown} />
       <div>
         <p className="italic lg:text-[0.8vw] xl:text-sm text-xs md:block hidden text-center w-[90%] mx-auto">
           We specialize in providing skilled professionals across industries
           like construction, oil and gas, hospitality, and more, we’re here to
           help you find the right talent.
         </p>
-        <h3 className="font-medium lg:text-[1.2vw] xl:text-xl text-xs text-center py-4 ">
+        <h3 className="font-medium lg:text-[1.2vw] xl:text-xl text-xs text-center md:py-4 py-2 ">
           Contact us today to access top talent for your business needs.
         </h3>
 
